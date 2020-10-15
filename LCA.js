0c04ad31839db636829dbaef9ce17e2d5ce11c30
leonawolff <leona.wolff.ok@gmail.com>
2020-10-15 15:09:45 +0100

    Update LCA.js

--- LCA.js
+++ LCA.js
@@ -43,12 +43,24 @@ current.children = [new Node(14), new Node(5)];
 current = current.children[0];
 current.children = [new Node(8)];
 
+/* Tree should look like this:
 
+                             (0)
+                       /      \       \
+                   (9)         (2       (23)
+                 /   \          \       /   \
+              (3)  (1)          (18)  (22)   (19)
+            /     /   \         /  \        /   \
+          (7)   (4)   (11)    (26) (30)   (14)  (5)
+      /   |   \       /  \                        \
+    (25) (12) (17)  (24) (6)                      (8)
+
+*/
 
 // doin bits
 
-var key1 = 6;
-var key2 = 3;
+var key1 = 14;
+var key2 = 5;
 
 if(searchTree(root, key1) && searchTree(root, key2)){
 
@@ -62,42 +74,27 @@ else{
     console.log("The lowest common ancestor of " + key1 + " and " + key2 + " could not be found.");
 }
 
-
-
 // LCA section
 
 function lca(current, key1, key2, lcaTemp){
 
-//  console.log("aaaaaaaaaaa " + current.key);
+  console.log("aaaaaaaaaaa " + current.key);
 
-  if(searchTree(key1) && searchTree(key2)){
+  if(searchTree(current, key1) && searchTree(current, key2)){
+
     lcaTemp = current.key;
     var i;
-    for(i = 0; i < current.children.length - 1; i++){
-      lca(current.children[i], key1, key2, lcaTemp);
+    for(i = 0; i < current.children.length; i++){
+      lcaTemp = lca(current.children[i], key1, key2, lcaTemp);
     }
   }
+  return lcaTemp;
 }
 
 // check if a node with the given key exists in the tree
-/* Tree should look like this:
 
-                             (0)
-                       /      \       \
-                   (9)         (2       (23)
-                 /   \          \       /   \
-              (3)  (1)          (18)  (22)   (19)
-            /     /   \         /  \        /   \
-          (7)   (4)   (11)    (26) (30)   (14)  (5)
-      /   |   \       /  \                        \
-    (25) (12) (17)  (24) (6)                      (8)
-
-
-*/
 function searchTree(root, key){
 
-//  console.log(root.key);
-//  console.debug(root.children)
   let found = false;
   var i;
   if(root.children && root.children.length > 0){
@@ -116,31 +113,3 @@ function searchTree(root, key){
   }
   return false;
 }
-
-// function searchTree(root, key){
-//   // console.log("Root children on searchTree entry: ");
-//   // console.debug(root.children);
-//   // console.log("root key: " + root.key);
-//   let current = root;
-//   console.log(root.key);
-//   let found = false;
-//
-//   if(current.children.length > 0){
-//     var i;
-//     for(i = 0; i < current.children.length - 1; i++){
-//       console.log("current children");
-//       console.debug(current.children);
-//       if(current.children[i].key === key){
-//         return true;
-//       }
-//
-//     }
-//     for(i = 0; i < current.children.length - 1; i++){
-//       found = searchTree(current.children[i], key);
-//       if(found === true){
-//         return found;
-//       }
-//       }
-//     }
-//   return false;
-// }
