923ec07fdc48d99beca0490baff9fd7e16fb7837
leonawolff <leona.wolff.ok@gmail.com>
2020-10-22 10:47:32 +0100

    Update LCA.test.js

--- LCA.test.js
+++ LCA.test.js
@@ -1,61 +1,68 @@
-//import { Node, findLCA} from './LCA.js';
+var lcaFile= require('./LCA.js');
 
-//making the tree
+const root = lcaFile.createNode(0);
 
-const Node = require('./LCA.js');
-const lca = require('./LCA.js');
-
-const root = new Node(0);
-
-root.children = [new Node(9), new Node(2), new Node(23)];
+root.children = [lcaFile.createNode(9), lcaFile.createNode(2), lcaFile.createNode(23)];
 
 let current = root.children[0];
-current.children = [new Node(3), new Node(1)];
+current.children = [lcaFile.createNode(3), lcaFile.createNode(1)];
 
 current = current.children[0];
-current.children = [new Node(7)];
+current.children = [lcaFile.createNode(7)];
 
 current = current.children[0];
-current.children = [new Node(25), new Node(12), new Node(17)];
+current.children = [lcaFile.createNode(25), lcaFile.createNode(12), lcaFile.createNode(17)];
 
 current = root.children[0];
 current = current.children[1];
-current.children = [new Node(4), new Node(11)];
+current.children = [lcaFile.createNode(4), lcaFile.createNode(11)];
 
 current = current.children[1];
-current.children = [new Node(24), new Node(6)];
+current.children = [lcaFile.createNode(24), lcaFile.createNode(6)];
 
 current = root.children[1];
-current.children = [new Node(18)];
+current.children = [lcaFile.createNode(18)];
 
 current = current.children[0];
-current.children = [new Node(26), new Node(30)];
+current.children = [lcaFile.createNode(26), lcaFile.createNode(30)];
 
 current = root.children[2];
 
-current.children = [new Node(22), new Node(19)];
+current.children = [lcaFile.createNode(22), lcaFile.createNode(19)];
 
 current = current.children[1];
-current.children = [new Node(14), new Node(5)];
+current.children = [lcaFile.createNode(14), lcaFile.createNode(5)];
 
 current = current.children[0];
-current.children = [new Node(8)];
+current.children = [lcaFile.createNode(8)];
 
 /* Tree should look like this:
 
                              (0)
-                       /      \       \
-                   (9)         (2       (23)
-                 /   \          \       /   \
-              (3)  (1)          (18)  (22)   (19)
-            /     /   \         /  \        /   \
-          (7)   (4)   (11)    (26) (30)   (14)  (5)
-      /   |   \       /  \                        \
-    (25) (12) (17)  (24) (6)                      (8)
+                        /      |      \
+                   (9)         (2)       (23)
+                 /   \          \        /   \
+              (3)  (1)          (18)   (22)   (19)
+            /     /   \         /  \          /   \
+          (7)   (4)   (11)    (26) (30)      (14)  (5)
+      /   |   \       /  \                           \
+    (25) (12) (17)  (24) (6)                         (8)
 
 */
 
 
 test('Find LCA of 14 and 5', () => {
-  expect(lca(root, 14, 5)).toBe(19);
+  expect(lcaFile.lca(root, 14, 5)).toBe(19);
+});
+
+test('Find LCA of 14 and 5', () => {
+  expect(lcaFile.lca(root, 25, 8)).toBe(0);
+});
+
+test('Find LCA of 14 and 5', () => {
+  expect(lcaFile.lca(root, 30, 31)).toBe(null);
+});
+
+test('Find LCA of 14 and 5', () => {
+  expect(lcaFile.lca(root, 26, 30)).toBe(18);
 });
